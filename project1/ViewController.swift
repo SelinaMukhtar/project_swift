391136e479076094090d8343f129b3d908cfdc4e
Selina M. <selina_mukhtar@Selinas-MacBook-Pro.local>
2026-01-16 21:30:43 +0500

    comment

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var textfield: UITextField!
+    
+    @IBOutlet weak var textfield2: UITextField!
+    
+    
+    
+    @IBAction func plus(_ sender: Any) {
+        let a = textfield.text!
+        let b = textfield2.text!
+        
+        if let numberA = Int(a), let numberB = Int(b) {
+            let sum = numberA + numberB
+            label.text = String(sum)
+        }
+    }
+    
+    
+    @IBAction func minus(_ sender: Any) {
+        let a = textfield.text!
+        let b = textfield2.text!
+        
+        if let numberA = Int(a), let numberB = Int(b) {
+            let sum = numberA - numberB
+            label.text = String(sum)
+        }
+    }
+    
+    
+    @IBAction func multiply(_ sender: Any) {
+        let a = textfield.text!
+        let b = textfield2.text!
+        
+        if let numberA = Int(a),let numberB = Int(b) {
+            let sum = numberA * numberB
+            label.text = String(sum)
+        }
+    }
+    
+    
+    @IBAction func divide(_ sender: Any) {
+        
+        let a = textfield.text!
+        let b = textfield.text!
+        
+        if let numberA = Int(a), let numberB = Int(b) {
+            let sum = numberA / numberB
+            label.text = String(sum)
+        }
+    }
 }
 
